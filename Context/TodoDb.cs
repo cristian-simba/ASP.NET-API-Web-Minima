637950b58bc8375ab77e765203655f8e2be644a1
cristian-simba <davidsimba200@gmail.com>
2024-11-21 13:29:00 -0500

    DOT

---
+++ Context/TodoDb.cs
@@ -0,0 +1,14 @@
+﻿using ApiWebMinima.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWebMinima.Context
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options): base (options) 
+        {
+            
+        }
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
---
+++ Models/TodoItemDTO.cs
@@ -0,0 +1,25 @@
+﻿/*Se puede usar un DTO para:
+
+Evitar el exceso de publicación.
+Ocultar las propiedades que los clientes no deben ver.
+Omitir algunas propiedades para reducir el tamaño de la carga.
+Acoplar los gráficos de objetos que contienen objetos anidados. Los gráficos de objetos acoplados pueden ser más cómodos para los clientes.
+
+ */
+
+// DTO
+namespace ApiWebMinima.Models
+{
+    public class TodoItemDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public TodoItemDTO()
+        {
+            
+        }
+        public TodoItemDTO(Todo todo) => 
+            (Id, Name, IsComplete) = (todo.Id, todo.Name, todo.IsComplete);
+    }
+}
